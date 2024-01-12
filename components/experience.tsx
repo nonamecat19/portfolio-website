75ef9532ecdcbd053d41c47923d5249f0153b983
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-01-13 01:38:49 +0200

    feat: experience section improvements

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, {Fragment} from "react";
 import SectionHeading from "./section-heading";
 import {
   VerticalTimeline,
@@ -15,26 +15,27 @@ export default function Experience() {
   const { ref } = useSectionInView("Experience");
   const { theme } = useTheme();
 
+	const isLight = theme === "light"
+
 	return (
 		<section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
 			<SectionHeading>My experience</SectionHeading>
 			<VerticalTimeline lineColor="">
 				{experiencesData.map(
 					({
-						date,
-						location,
-						icon,
 						title,
-						description,
+						location,
+						company,
+						type,
 						stack,
 						paragraphs,
-						company
+						icon,
+						date,
 					 }, index) => (
-						<React.Fragment key={index}>
+						<Fragment key={index}>
 							<VerticalTimelineElement
 								contentStyle={{
-									background:
-										theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
+									background: isLight ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
 									boxShadow: "none",
 									border: "1px solid rgba(0, 0, 0, 0.05)",
 									textAlign: "left",
@@ -49,19 +50,33 @@ export default function Experience() {
 								date={date}
 								icon={icon}
 								iconStyle={{
-									background:
-										theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
+									background: isLight ? "white" : "#04785720",
+									color: "#34d399",
 									fontSize: "1.5rem",
+									border: "4px solid #10b981"
 								}}
 							>
-								<h3 className="font-semibold capitalize">{title}</h3>
-								<p className="font-normal !mt-0">{location}</p>
-								<p className="font-normal !mt-0">{company}</p>
-								<p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
-									{description}
+								<h3 className="font-bold capitalize text-xl mb-2 text-emerald-700 dark:text-emerald-500">
+									{title}
+								</h3>
+								<p className="font-normal !mt-0">
+									{company} ▼ {type}
 								</p>
+								<p className="!mt-0 !mb-1">
+									Tech stack: <strong className="text-emerald-700 dark:text-emerald-400">{stack}</strong>
+								</p>
+								{
+									paragraphs.map((paragraph, index) => (
+										<p key={index} className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
+											<strong className="text-emerald-700 dark:text-emerald-400">►</strong>{paragraph}
+										</p>
+									))
+								}
+								<span className="absolute right-0 top-0 bg-emerald-800 py-1.5 px-3 rounded-bl-2xl text-white">
+									{location}
+								</span>
 							</VerticalTimelineElement>
-						</React.Fragment>
+						</Fragment>
 					),
 				)}
 			</VerticalTimeline>
--- lib/data.ts
+++ lib/data.ts
@@ -1,7 +1,5 @@
 import React from "react";
-import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
+import { FaReact, FaVuejs } from "react-icons/fa";
 import corpcommentImg from "@/public/corpcomment.png";
 import rmtdevImg from "@/public/rmtdev.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
@@ -35,50 +33,51 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
-    description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-    icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    title: "Full Stack JS developer",
+    location: "Zhytomyr | On-site",
+    company: "4K Soft",
+    type: "Full-time",
+    stack: "Vue, Nuxt, NestJS, Postgres",
+    paragraphs: [
+      "Engaged in software development and support on many projects",
+      "Transferred the development team and projects to modern technologies (Vue3, Vite, Composition API, Pinia, TailwindCSS, HeadlessUI, Storybook, Vue-Query, Zod, FSD Architecture)",
+      "Worked as a mentor, trained people for commercial development",
+      "Took part in the development of high-load projects"
+    ],
+    icon: React.createElement(FaVuejs),
+    date: "May 2023 - January 2024",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
-    description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-    icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
-  },
-  {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
-    description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+    title: "Full Stack Developer",
+    location: "Zhytomyr | Remote",
+    company: "Zhytomyr Polytechnic State University",
+    type: "Part-time",
+    stack: "React, React Native, Capacitor, ExpressJS, ASP.Net, Postgres",
+    paragraphs: [
+      "Developed prototype of a system for monitoring the health of university students",
+      "Created mobile applications from existing web application (PWA)"
+    ],
     icon: React.createElement(FaReact),
-    date: "2021 - present",
-  },
+    date: "March 2023 - January 2024",
+  }
 ] as const;
 
 export const projectsData = [
   {
     title: "CorpComment",
-    description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
+    description: "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
     tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
     imageUrl: corpcommentImg,
   },
   {
     title: "rmtDev",
-    description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
+    description: "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
     tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
     imageUrl: rmtdevImg,
   },
   {
     title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    description: "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
     tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
     imageUrl: wordanalyticsImg,
   },
