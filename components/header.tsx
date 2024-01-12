6530a4e3eaa1734b986687e7768ef0385adb7d95
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-01-12 13:55:46 +0200

    add: navigation improvement

--- components/header.tsx
+++ components/header.tsx
@@ -21,32 +21,32 @@ export default function Header() {
 
       <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
-          {links.map((link) => (
+          {links.map(({hash, name}) => (
             <motion.li
               className="h-3/4 flex items-center justify-center relative"
-              key={link.hash}
+              key={hash}
               initial={{ y: -100, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
             >
               <Link
                 className={clsx(
-                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
+                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300 duration-150",
                   {
-                    "text-gray-950 dark:text-gray-200":
-                      activeSection === link.name,
+                    "text-gray-200 hover:text-gray-200 dark:text-gray-100":
+                      activeSection === name,
                   }
                 )}
-                href={link.hash}
+                href={hash}
                 onClick={() => {
-                  setActiveSection(link.name);
+                  setActiveSection(name);
                   setTimeOfLastClick(Date.now());
                 }}
               >
-                {link.name}
+                {name}
 
-                {link.name === activeSection ? (
+                {name === activeSection ? (
                   <motion.span
-                    className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
+                    className="bg-teal-700 rounded-full absolute inset-0 -z-10 dark:bg-emerald-800"
                     layoutId="activeSection"
                     transition={{
                       type: "spring",
